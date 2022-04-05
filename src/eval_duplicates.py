4f58a3fcb23baef6fc77356c727e87c93619d0a1
ZephyrII <tomasz.no@pm.me>
2022-04-05 18:11:01 +0200

    AMCS

--- src/eval_duplicates.py
+++ src/eval_duplicates.py
@@ -5,7 +5,7 @@ import signal
 from tqdm import tqdm
 import rosbag
 
-models = ["c_hrnet48_udp_512_hm128_c2","c_hrnet48_udp_512_hm512"]
+models = ["AMCS_kpts"]
 BASE_DIR = "/root/share/tf/results_2.0/"
 paths = ["2020-11-09", "2020-11-13", "2020-11-25", "2020-12-14", "2020-12-16"]
 # paths = ["/home/tomasz/share_docker/eval_ds_crops/2020-12-16"]
@@ -31,7 +31,7 @@ for model in models:
                                     total_frames +=1
                                     for pair in pairs:
                                             dist = np.linalg.norm(kpts[pair[0]]-kpts[pair[1]])
-                                            if dist<2:
+                                            if dist<10:
                                                     dup = True
                                                     # print(msg)
                                     if dup:
--- src/eval_rosbags.py
+++ src/eval_rosbags.py
@@ -3,10 +3,11 @@ import subprocess
 import signal
 from tqdm import tqdm
 
-paths = ["/root/share/tf/dataset/2020-11-09", "/root/share/tf/dataset/2020-11-13", "/root/share/tf/dataset/2020-11-25", "/root/share/tf/dataset/2020-12-14", "/root/share/tf/dataset/2020-12-16"]
+# paths = ["/root/share/tf/dataset/2020-11-09", "/root/share/tf/dataset/2020-11-13", "/root/share/tf/dataset/2020-11-25", "/root/share/tf/dataset/2020-12-14", "/root/share/tf/dataset/2020-12-16"]
+paths = ["/root/share/tf/dataset/eval_ds_clean/2020-11-09", "/root/share/tf/dataset/eval_ds_clean/2020-11-13", "/root/share/tf/dataset/eval_ds_clean/2020-11-25", "/root/share/tf/dataset/eval_ds_clean/2020-12-14", "/root/share/tf/dataset/eval_ds_clean/2020-12-16"]
 
 out_path = "/root/share/tf/results_2.0"
-version = "c_hrnet48_udp_512_hm512"
+version = "AMCS_kpts" #ROSBAG SPEED .25!!!!
 for path in tqdm(paths, "Dirs"):
         os.chdir(path)
         day = path.split('/')[-1]
@@ -16,7 +17,7 @@ for path in tqdm(paths, "Dirs"):
 
         for filename in tqdm(files, "Files", leave=False):
                 if filename.endswith(".bag"):                      
-                        play_command = "rosbag play --topics /dgps_ublox/dgps_base/fix /dgps_ublox/dgps_rover/navrelposned  /processed_can_data /blackfly/camera/image_color/compressed -r 0.3 -s 0 " +os.path.join(path, filename)
+                        play_command = "rosbag play --topics /dgps_ublox/dgps_base/fix /dgps_ublox/dgps_rover/navrelposned  /processed_can_data /blackfly/camera/image_color/compressed -r 0.004 -s 0 " +os.path.join(path, filename)
                         record_command = "rosbag record /dgps_ublox/dgps_base/fix /dgps_ublox/dgps_rover/navrelposned /pose_estimator/keypoints /processed_can_data -O " + os.path.join(out_path, version, day, filename[:-4]+"_det.bag")
                         play_process = subprocess.Popen(play_command, stdin=subprocess.PIPE, shell=True)
                         record_process = subprocess.Popen(record_command, stdin=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
@@ -32,7 +33,8 @@ for path in tqdm(paths, "Dirs"):
         files = os.listdir(path)
         for filename in tqdm(files, "Files", leave=False):
                 if filename.startswith(("_2020-11-09-12-43-04")):                       
-                        play_command = "rosbag play --topics /dgps_ublox/dgps_base/fix /dgps_ublox/dgps_rover/navrelposned  /processed_can_data /blackfly/camera/image_color/compressed -r 0.3 -s 0 " +os.path.join(path, filename)
+                # if filename.startswith(("_2020-11-13-13-43-53")):                       
+                        play_command = "rosbag play --topics /dgps_ublox/dgps_base/fix /dgps_ublox/dgps_rover/navrelposned  /processed_can_data /blackfly/camera/image_color/compressed -r 0.2 -s 0 " +os.path.join(path, filename)
                         record_command = "rosbag record /dgps_ublox/dgps_base/fix /dgps_ublox/dgps_rover/navrelposned /pose_estimator/keypoints /processed_can_data -O " + os.path.join(out_path, version, day, filename[:-4]+"_det.bag")
                         play_process = subprocess.Popen(play_command, stdin=subprocess.PIPE, shell=True)
                         record_process = subprocess.Popen(record_command, stdin=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
